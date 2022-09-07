eccd95957f8331f010950041351d2d05c70eb676
Yujie Guo <3196813180@qq.com>
2022-09-07 14:18:26 +0800

    test_commit

---
+++ demo.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+//#include <cmath>
+//#include <iomanip>
+//#include <cstring>
+//#include <cstdlib>
+//#include <string>
+//#include <algorithm>
+//#include <vector>
+//#include <list>
+//#include <queue>
+//#include <stack>
+//#include <set>
+//#include <map>
+using namespace std;
+
+int main()
+{
+    string s = "213";
+    s = "x";
+    cout<<s;
+
+	system("pause");
+	return 0;
+}
